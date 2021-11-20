7d5e815e84c7d0e620a5ae3e687c446704929904
GQDeltex <operitum@gmail.com>
2021-11-20 17:34:18 +0100

    First working version of the proxy

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
-	"time"
-
+	"errors"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cache"
 	log "github.com/sirupsen/logrus"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
 )
 
 func main() {
@@ -20,8 +24,36 @@ func main() {
 	}))
 
 	// The main route
-	app.Get("/:url", func(c *fiber.Ctx) error {
-		return c.SendString("OK")
+	app.Get("/*", func(c *fiber.Ctx) error {
+		urlstr := c.Params("*")
+		if urlstr == "" {
+			log.Error("No url Parameter")
+			return errors.New("No URl parameter")
+		}
+		url, err := url.Parse(urlstr)
+		if err != nil {
+			log.Error(err)
+			return err
+		}
+		log.Debug("URL:", url.String())
+		if url.Hostname() == "" {
+			log.Error("No Host was given")
+			return errors.New("No hostname was given")
+		}
+		resp, err := http.Get(url.String())
+		if err != nil {
+			log.Error(err)
+			return err
+		}
+		//We Read the response body on the line below.
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		content_type := resp.Header["Content-Type"][0]
+		content_type = strings.Split(content_type, "/")[1]
+		log.Debug("Content-Type:", content_type)
+		return c.Status(resp.StatusCode).Type(content_type).Send(body)
 	})
 
 	// Start the Webserver
